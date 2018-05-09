2f81a19f33222f7db716a15b4c9b590c6c255a90
stvyzxx <yurasobechko@gmail.com>
2018-05-09 14:38:15 +0300

    finished router state composition

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,6 +1,5 @@
 import {
   ActionReducerMap,
-  createSelector,
   createFeatureSelector
 } from '@ngrx/store';
 import * as fromPizzas from './pizza.reducer';
@@ -16,18 +15,3 @@ export const reducers: ActionReducerMap<ProductsState> = {
 export const getProductsState = createFeatureSelector<ProductsState>(
   'products'
 );
-
-// pizzas state
-export const getPizzasState = createSelector(
-  getProductsState,
-  (state: ProductsState) => state.pizzas
-);
-
-export const getPizzasEntities = createSelector(getPizzasState, fromPizzas.getPizzasEntities);
-export const getAllPizzas = createSelector(getPizzasEntities,
-  entities => {
-    return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
-  }
-);
-export const getPizzasLoaded = createSelector(getPizzasState, fromPizzas.getPizzasLoaded);
-export const getPizzasLoading = createSelector(getPizzasState, fromPizzas.getPizzasLoading);

803ec8ab7e5a1d4f3412ddbf160c877bd0f80556
stvyzxx <yurasobechko@gmail.com>
2018-05-12 16:23:04 +0300

    finished guards

--- src/products/guards/index.ts
+++ src/products/guards/index.ts
@@ -1,5 +1,9 @@
 import { PizzasGuard } from './pizzas.guard';
+import { PizzaExistsGuard } from './pizzas-exists.guard';
+import { ToppingsGuard } from './toppings.guard';
 
-export const guard: any = [PizzasGuard];
+export const guard: any = [PizzasGuard, PizzaExistsGuard, ToppingsGuard];
 
 export * from './pizzas.guard';
+export * from './pizzas-exists.guard';
+export * from './toppings.guard';
